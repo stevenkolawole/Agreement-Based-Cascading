0068ef5ac50cc206c9264177d18dd51c7bb98792
Steven Kolawole <skolawol@andrew.cmu.edu>
2024-09-18 22:19:40 -0400

    add frugalGPT's cascade class

--- src/methods/methods.py
+++ src/methods/methods.py
@@ -3,6 +3,7 @@ from concurrent.futures import ThreadPoolExecutor
 from time import time
 from collections import Counter
 import re
+from .frugalgpt_scorer import Scorer
 
 
 class CascadeMethod:
@@ -11,7 +12,7 @@ class CascadeMethod:
             ServiceProvider,
             TaskData,
             cascade_tier_models: List[Union[List[str], str]], # List of str or List of Lists denoting ensembles or single models cascades
-            temperature: Union[List[str], str] = 0.6
+            temperature: Union[List[float], float] = 0.6
     ):
         self.cascade_models = cascade_tier_models
         self.n_tiers = len(cascade_tier_models)
@@ -74,7 +75,7 @@ class EnsembleCascade(CascadeMethod):
             ServiceProvider,
             TaskData,
             cascade_tier_models: List[Union[List[str], str]], 
-            temperature: Union[List[str], str] = 0.6,
+            temperature: float = 0.6,
             agreement_threshold: float = 1.0 # (2/3) or full agreement = 1.0
     ):
         super().__init__(ServiceProvider, TaskData, cascade_tier_models, temperature)
@@ -91,12 +92,12 @@ class EnsembleCascade(CascadeMethod):
                 f_responses = extract_answer(responses, self.Task.label_regex)
                 majority_answer, majority_count = Counter(f_responses).most_common(1)[0]
                 consistency = (majority_count / len(f_responses)) >= self._threshold
-#                print(majority_answer, majority_count)
-#                print(responses)
-#                print(f_responses)
+                print(majority_answer, majority_count)
+                print(responses)
+                print(f_responses)
                 if consistency and majority_answer != "": 
                     break
-#            print("Exiting at tier ", tier)
+            print("Exiting at tier ", tier)
             self.total_latency += time() - start_time
             answers.append(majority_answer)
         return answers, self.total_latency / len(prompts)
@@ -107,8 +108,8 @@ class MOTLLMCascade(EnsembleCascade):
         self,
         ServiceProvider,
         TaskData,
-        cascade_tier_models: List[Union[List[str], str]],
-        temperature: Union[List[str], str] = [0.4, 0.6, 0.8],
+        cascade_tier_models: List[str],
+        temperature: List[float] = [0.4, 0.6, 0.8],
         mixture_consistency_threshold: float = 2/3 # (2/3) or full consistency check = 1.0
     ):
         super().__init__(ServiceProvider, TaskData, cascade_tier_models, temperature)
@@ -116,8 +117,94 @@ class MOTLLMCascade(EnsembleCascade):
 
 
 class FrugalGPT(CascadeMethod):
-    pass
+    def __init__(
+            self,
+            ServiceProvider,
+            TaskData,
+            cascade_tier_models: List[str], # List of str or List of Lists denoting ensembles or single models cascades
+            temperature: float = 0.6,
+            train: bool = False,
+    ):
+        super().__init__(ServiceProvider, TaskData, cascade_tier_models, temperature)
+        self.tools = {}
+        self._thresholds = (0.96, 0.37) # hardcoded from the paper
+        if train:
+            print("Training FrugalGPT's scorer functions...")
+            self.train_scorer()
+            print("Training complete!")
+        else:
+            pass # print("Loading complete!")
 
+    def train_scorer(self):
+        for tier in range(self.n_tiers):
+            if tier != self.n_tiers - 1: # don't train for last tier
+                print(f"Training the scoring function for {self.cascade_models[tier]}...")
+                self.tools[tier] = {}
+                self.tools[tier]['threshold'] = self._thresholds[tier]
+                self.tools[tier]['FOLDER'] = f"scorer_logs/{self.data_url.split('/')[-1]}_{tier}/"
+                self.tools[tier]['Scorer'] = self._train_scorer(tier)
+        self.setup_cost = self.total_cost # inference costs for training; does not include GPU cost
+        self.total_cost = 0 # start total cost afresh
+
+    def _train_scorer(self, tier):
+        self._process_data_for_training(tier)
+        return self._train_on_processed_data(tier)
+
+    def _process_data_for_training(self, tier):
+        temp_data = self.Task.train_data.train_test_split(test_size=.4)
+        self._temp_train, self._temp_val = temp_data['train'], temp_data['test']
+        self._temp_train = self._generate_label_process_data(tier, self._temp_train)
+        self._temp_val = self._generate_label_process_data(tier, self._temp_val)
+    
+    def _generate_label_process_data(self, tier, data):
+        raw_responses, preds = [], []
+        for prompt in data[self.Task.query_column]:
+            response = self.generate_inference(prompt, self.cascade_models[tier])
+            raw_responses.append(response)
+            preds.append(extract_answer(response, self.Task.label_regex))
+
+        labels = data[self.Task.label_column]
+        if self.Task.groundtruth_need_regex:
+            labels = extract_answer(labels, self.Task.label_regex)
+
+        def compute_quality(example, idx):
+            return {
+                "query": f"{example[self.query_column]}\nA: {raw_responses[idx]}",
+                "label": int(str(labels[idx]) == preds[idx])
+            }
+        data = self.map(
+            compute_quality, 
+            with_indices=True,
+            remove_columns=data.column_names
+        )
+        return data
+
+    def _train_on_processed_data(self, tier):
+        scorer = Scorer(TASK_FOLDER=self.tools[tier]['FOLDER'])
+        scorer.pipeline(self._temp_train,
+                        self._temp_val,
+                        "query",)
+        return scorer
+
+    def _inference_cascade(self, prompts: List[str]) -> Tuple[List[str], float]:
+        answers = []
+        for prompt in prompts:
+            start_time = time()
+            for tier in range(self.n_tiers):
+                response = self.generate_inference(prompt=prompt, models=self.cascade_models[tier])
+                if tier != self.n_tiers - 1: # we don't need the check for the last tier
+                    score = self.tools[tier]['Scorer'].get_score(response)
+                    consistency = score > self.tools[tier]['threshold']
+                    print(score)
+                    print(response)
+                    if consistency: 
+                        break
+            print("Exiting at tier ", tier)
+            f_response = extract_answer(response, self.Task.label_regex)
+            answers.append(f_response)
+            self.total_latency += time() - start_time
+        return answers, self.total_latency / len(prompts)
+    
 
 class AutoMix(CascadeMethod):
     pass
@@ -138,7 +225,7 @@ def extract_answer(raw_responses: Union[List[str], str], regex_pattern: str) -> 
                 responses.append("")
                 
         except AttributeError:
-            print("Answer not found in ==> `{r}`\n\n") # add f-string later
+            print(f"Answer not found in ==> `{r}`\n")
             responses.append("")
             
     return responses
