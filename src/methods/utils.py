d8ec9eb68882ead1cbc7306293dd2bfe6257f8f4
Steven Kolawole <skolawol@andrew.cmu.edu>
2024-10-09 02:30:15 -0400

    add F1 eval for Automix and CoQA

--- src/methods/utils.py
+++ src/methods/utils.py
@@ -1,5 +1,6 @@
 from typing import List, Tuple, Union
 import re
+from sklearn.metrics import f1_score
 
 
 def extract_answer(raw_responses: Union[List[str], str], regex_pattern: str) -> List[str]:
@@ -22,6 +23,23 @@ def extract_answer(raw_responses: Union[List[str], str], regex_pattern: str) -> 
             
     return responses
 
-def calculate_accuracy(pred: List[str], true_labels: List[str]) -> float:
-    correct_count = sum(p == str(t) for p, t in zip(pred, true_labels))
-    return correct_count / len(pred)
+def calculate_accuracy(preds: List[str], true_labels: List[str]) -> float:
+    correct_count = sum(p == str(t) for p, t in zip(preds, true_labels))
+    print("True labels: ", true_labels)
+    print("Predictions: ", preds)
+    return correct_count / len(preds)
+
+
+def calculate_f1(preds: List[str], true_labels: List[str]) -> float:
+    normalized_preds = [normalize_answer(pred) for pred in preds]
+    normalized_labels = [normalize_answer(label) for label in true_labels]
+    print("True labels: ", normalized_labels)
+    print("Predictions: ", normalized_preds)
+    return f1_score(normalized_labels, normalized_preds, average="macro")
+
+def normalize_answer(answer: str) -> str:
+    answer = answer.lower()
+    answer = re.sub(r'[^\w\s]', '', answer)
+    answer = re.sub(r'\b(the|a|an)\b', '', answer)
+    answer = re.sub(r'\s+', ' ', answer).strip()
+    return answer
